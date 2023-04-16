9ee49a8422892ef12454cccc09527dfeeb080932
kleinfour <shawnwilliambowser@gmail.com>
2023-04-15 18:24:41 -0700

    random dataframe

--- pages/map.py
+++ pages/map.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pydeck as pdk
+import pandas as pd
+import numpy as np
 import os
 import folium
 from folium.plugins import Draw
@@ -16,6 +18,8 @@ TERRAIN_IMAGE = "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}
 ELEVATION_DECODER = {"rScaler": 256, "gScaler": 1, "bScaler": 1 / 256, "offset": -32768}
 SURFACE_IMAGE = f"https://api.mapbox.com/v4/mapbox.satellite/{{z}}/{{x}}/{{y}}@2x.png?access_token={MAPBOX_API_KEY}"
 
+START_LOCATION = [46.24, -122.18]
+
 @st.cache_data
 def load_map():
     terrain_layer = pdk.Layer(
@@ -25,12 +29,18 @@ def load_map():
         elevation_data=TERRAIN_IMAGE
     )
 
-    view_state = pdk.ViewState(latitude=46.24, longitude=-122.18, zoom=11.5, bearing=140, pitch=60)
+    view_state = pdk.ViewState(latitude=START_LOCATION[0], longitude=START_LOCATION[1],
+                                zoom=11.5, bearing=140, pitch=60)
     r = pdk.Deck(terrain_layer, initial_view_state=view_state)
     return r
 
+# random dataframe displayed on map
+map_data = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + START_LOCATION,
+columns=['lat', 'lon'])
+
 # Folium map
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+m = folium.Map(location=START_LOCATION, zoom_start=10)
 Draw(export=True).add_to(m)
 output = st_folium(m, width=700, height=500)
 

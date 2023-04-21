b221f1c4c8dd1a10791a9f2d9553842d558cd581
kleinfour <shawnwilliambowser@gmail.com>
2023-04-21 16:19:19 -0700

    settings wrapped in .env.dev

--- config/settings.py
+++ config/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": "db",
-        "NAME": "postgres",
-        "PASSWORD": "postgres",
-        "PORT": 5432,
-        "USER": "postgres",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 

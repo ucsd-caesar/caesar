bf5cd74c027d16a64dc8f9432976b7d00aee3072
kleinfour <shawnwilliambowser@gmail.com>
2023-05-21 10:34:53 -0700

    add sidebar w/ 2 buttons

--- dashboard/static/dashboard/session.js
+++ dashboard/static/dashboard/session.js
@@ -3,13 +3,9 @@ const loginBtn = document.getElementById('login-btn');
 function updateLoginBtn(isAuthenticated, id) {
     if (isAuthenticated) {
         loginBtn.innerHTML = 'Profile';
-        loginBtn.addEventListener('click', () => {
-            window.location.href = '/dashboard/user/' + id;
-        });
+        loginBtn.href = '/dashboard/user/' + id;
     } else {
         loginBtn.innerHTML = 'Login';
-        loginBtn.addEventListener('click', () => {
-            window.location.href = '/dashboard/login';
-        });
+        loginBtn.href = '/dashboard/login';
     }
 }

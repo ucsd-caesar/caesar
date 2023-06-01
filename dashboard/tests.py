467b7b515bc656ea84d2c7633c2c9f6f108a5a1d
kleinfour <shawnwilliambowser@gmail.com>
2023-06-01 16:50:20 -0700

    users can publish stream at path==id -- some tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -11,6 +11,8 @@ from unittest.mock import patch
 from .models import *
 from .viewsets import *
 
+import requests
+import json
 import subprocess
 import logging
 logger = logging.getLogger(__name__)
@@ -18,6 +20,7 @@ logger = logging.getLogger(__name__)
 ADMIN_USER = "testAdmin"
 ADMIN_EMAIL = "testAdmin@admin.com"
 ADMIN_PASS = "admin"
+TIMEOUT = 2
 
 def create_stream(title, source, groups, created_by_id):
     """
@@ -52,7 +55,8 @@ class CustomUserAPITests(APITestCase):
     """
     Test suite for CustomUser
     """
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         """
         Set self data to be used in tests
         """
@@ -99,7 +103,8 @@ class LivestreamAPITests(APITestCase):
     """
     Test suite for Livestream
     """
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         """
         Set self data to be used in tests
         """
@@ -110,9 +115,9 @@ class LivestreamAPITests(APITestCase):
             "source": "testSource",
             "created_by_id": self.admin.id
         }
-        self.client.force_authenticate(user=self.admin)
 
     def test_create_public_livestream(self):
+        self.client.force_authenticate(user=self.admin)
         response = self.client.post(reverse('livestream-list'), self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -126,6 +131,7 @@ class LivestreamAPITests(APITestCase):
         self.assertEqual(newStream.groups.first().name, "Public")
 
     def test_create_private_livestream(self):
+        self.client.force_authenticate(user=self.admin)
         self.data["groups"] = ["Private"]
         response = self.client.post(reverse('livestream-list'), self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -140,6 +146,7 @@ class LivestreamAPITests(APITestCase):
         self.assertEqual(newStream.groups.first().name, "Private")
 
     def test_create_group_livestream(self):
+        self.client.force_authenticate(user=self.admin)
         self.data["groups"] = ["Admin"]
         reponse = self.client.post(reverse('livestream-list'), self.data, format='json')
         self.assertEqual(reponse.status_code, status.HTTP_200_OK)
@@ -154,6 +161,7 @@ class LivestreamAPITests(APITestCase):
         self.assertEqual(newStream.groups.first().name, "Admin")
 
     def test_create_multiple_types_livestream(self):
+        self.client.force_authenticate(user=self.admin)
         self.data["source"] = "rtsp://testSource"
         response = self.client.post(reverse('livestream-list'), self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -200,7 +208,8 @@ class StreamAuthenticationTests(TestCase):
     """
     Test suite for Livestream authentication
     """
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         """
         Set self data to be used in tests
         """
@@ -214,19 +223,35 @@ class StreamAuthenticationTests(TestCase):
             "is_admin": False,
             "viewports": []
         }
+
         self.user = create_user(data["username"], data["email"], data["password"], data["is_admin"])
+
+        # create path configuration for ADMIN_USER
+        with open('dev/json/path.json') as f:
+            r = requests.post("http://mediamtx:9997/v2/config/paths/add/" + str(self.admin.id), json=json.load(f))
+
+        print("Path configuration created for ADMIN_USER with id: " + str(self.admin.id))
+        
+        # url to return path configuration for ADMIN_USER
+        self.path_url = "http://mediamtx:9997/v2/paths/get/" + str(self.admin.id)
 
         # start celery worker
         with patch('subprocess.run') as mock:
             mock.return_value = None
-            subprocess.run(["celery", "-A", "config", "worker", "--loglevel=info"])
-            mock.assert_called_once_with(["celery", "-A", "config", "worker", "--loglevel=info"])
+            subprocess.run(["celery", "multi", "restart", "1", "-A", "config", "-l", "INFO", "-P", "eventlet", "-c", "1000"])
+            mock.assert_called_once_with(["celery", "multi", "restart", "1", "-A", "config", "-l", "INFO", "-P", "eventlet", "-c", "1000"])
 
     def test_create_livestream_no_auth(self):
         """
         Test that a user cannot create a livestream without authentication
         """
-        rtsp_url = "rtsp://mediamtx:8554/rices"
+        r = requests.get(self.path_url)
+        try:
+            r.json() # if json is returned, then the path exists
+        except requests.exceptions.JSONDecodeError:
+            raise Exception("Path does not exist")
+
+        rtsp_url = "rtsp://mediamtx:8554/" + str(self.admin.id)
         output = subprocess.run(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
         self.assertEqual(output.returncode, 1) # ffmpeg should fail to connect
 
@@ -234,44 +259,75 @@ class StreamAuthenticationTests(TestCase):
         """
         Test that a user cannot create a livestream with wrong password
         """
-        rtsp_url = "rtsp://" + ADMIN_USER + ":wrongpass@mediamtx:8554/rices"
+        r = requests.get(self.path_url)
+        try:
+            r.json() # if json is returned, then the path exists
+        except requests.exceptions.JSONDecodeError:
+            raise Exception("Path does not exist")
+
+        rtsp_url = "rtsp://" + ADMIN_USER + ":wrongpass@mediamtx:8554/" + str(self.admin.id)
+        output = subprocess.Popen(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
+        try:
+            # subprocess should timeout
+            output.communicate(timeout=TIMEOUT)
+            self.assertEqual(output.returncode, 1)
+        except subprocess.TimeoutExpired:
+            output.terminate()  
+    
+    def test_create_livestream_no_path(self):
+        """
+        Test that a user cannot create a livestream at a path that does not exist
+        """
+        non_path_url = "http://mediamtx:9997/v2/paths/get/nonPath"
+        r = requests.get(non_path_url)
+        try:
+            r.json() # if json is returned, then the path exists
+            raise Exception("Path exists")
+        except requests.exceptions.JSONDecodeError:
+            self.assertEqual(r.status_code, status.HTTP_404_NOT_FOUND) # the path should not exist
+
+        rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/nonPath"
         output = subprocess.run(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
-        self.assertEqual(output.returncode, 1)
+        self.assertEqual(output.returncode, 1) # ffmpeg should fail to connect
 
-    # below tests which create a path with config other than 'path.json' result in bad request - tests hang
-    #
-    # def test_create_livestream_no_path(self):
-    #     """
-    #     Test that a user cannot create a livestream at a path that does not exist
-    #     """
-    #     rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/rices"
-    #     output = subprocess.run(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
-    #     self.assertEqual(output.returncode, 1)
+    def test_create_livestream_wrong_path(self):
+        """
+        Test that a user cannot create a livestream at a path that is not theirs
+        """
+        r = requests.get(self.path_url)
+        try:
+            r.json() # if json is returned, then the path exists
+        except requests.exceptions.JSONDecodeError:
+            raise Exception("Path does not exist")
 
-    # def test_create_livestream_wrong_path(self):
-    #     """
-    #     Test that a user cannot create a livestream at a path that is not theirs
-    #     """
-    #     with patch('subprocess.run') as mock:
-    #         mock.return_value = None
-    #         # add new path configuration for testUser
-    #         subprocess.run(["http", "--form", "post", "mediamtx:9997/v2/config/paths/add/rices", "<", "dev/json/userpathconf.json"])
-    #         mock.assert_called_once_with(["http", "--form", "post", "mediamtx:9997/v2/config/paths/add/rices", "<", "dev/json/path.json"])
-
-    #     rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/rices"
-    #     output = subprocess.run(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
-    #     self.assertEqual(output.returncode, 1)
+        rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/" + str(self.admin.id + 1)
+        output = subprocess.Popen(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
+        try:
+            # subprocess should timeout
+            output.communicate(timeout=TIMEOUT)
+            self.assertEqual(output.returncode, 1)
+        except subprocess.TimeoutExpired:
+            output.terminate()  
 
     # def test_create_livestream_correct(self):
     #     """
     #     Test that a user can create a livestream at an authorized path with correct password
     #     """
-    #     with patch('subprocess.run') as mock:
-    #         mock.return_value = None
-    #         # add new path configuration for testAdmin
-    #         subprocess.run(["http", "--form", "post", "mediamtx:9997/v2/config/paths/add/rices", "<", "dev/json/adminpathconf.json"])
-    #         mock.assert_called_once_with(["http", "--form", "post", "mediamtx:9997/v2/config/paths/add/rices", "<", "dev/json/path.json"])
-    #     rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/rices"
-    #     output = subprocess.run(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
-    #     self.assertEqual(output.returncode, 0)
+    #     r = requests.get(self.path_url)
+    #     try:
+    #         r.json() # if json is returned, then the path exists
+    #         print(r.json())
+    #     except requests.exceptions.JSONDecodeError:
+    #         raise Exception("Path does not exist")
+        
+    #     rtsp_url = "rtsp://" + ADMIN_USER +":"+ ADMIN_PASS + "@mediamtx:8554/" + str(self.admin.id)
+    #     print("RTSP URL: " + rtsp_url)
+    #     output = subprocess.Popen(["ffmpeg", "-re", "-stream_loop", "-1", "-i", "dev/media/Rices.ts", "-c:v", "copy", "-c:a", "aac", "-bsf:a", "aac_adtstoasc", "-f", "rtsp", "-rtsp_transport", "tcp", rtsp_url])
+    #     try:
+    #         output.communicate(timeout=TIMEOUT)
+    #         print(output)
+    #         self.assertEqual(output.returncode, 0) # subprocess should not timeout
+    #     except subprocess.TimeoutExpired:
+    #         output.terminate()
+    #         raise Exception("Livestream did not start")
 
